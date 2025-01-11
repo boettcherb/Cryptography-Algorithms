b35d67d9c19b05e82b35b66f56ab601502834ae3
Brandon Boettcher <brandonboettcher19@gmail.com>
2025-01-11 15:44:53 -0600

    Add accepted_args list as input to parse_args

--- io.cpp
+++ io.cpp
@@ -4,22 +4,35 @@
 #include <sstream>
 #include <iterator>
 #include <iomanip>
+#include <algorithm>
 
 /*
  * Parse the command line arguments into a map of argument names to values.
  * Arguments should be in the form "--name=value".
+ * 
+ * argc: The number of command line arguments.
+ * argv: The array of command line arguments.
+ * accepted_args: A list of valid argument names.
+ * usage: A usage string to be displayed if the user provides incorrect arguments.
  */
-std::map<std::string, std::string> parse_args(int argc, char** argv, const std::string& usage) {
+std::map<std::string, std::string> parse_args(int argc, char** argv,
+    const std::vector<std::string>& accepted_args, const std::string& usage) {
     std::map<std::string, std::string> args;
     for (int i = 1; i < argc; ++i) {
         std::string arg = argv[i];
         size_t equals_pos = arg.find('=');
-        if (equals_pos == std::string::npos) {
+        if (equals_pos == std::string::npos || arg.find("--") != 0) {
             std::cerr << "Error: Invalid argument: " << arg << std::endl;
             std::cerr << usage << std::endl;
             exit(1);
         }
         std::string name = arg.substr(2, equals_pos - 2);
+        if (std::find(accepted_args.begin(), accepted_args.end(), name)
+            == accepted_args.end()) {
+            std::cerr << "Error: Invalid argument: " << name << std::endl;
+            std::cerr << usage << std::endl;
+            exit(1);
+        }
         if (args.find(name) != args.end()) {
             std::cerr << "Error: Duplicate argument: " << name << std::endl;
             std::cerr << usage << std::endl;
@@ -61,7 +74,8 @@ void write_file(const std::string& filename, const std::string& contents) {
 std::string to_hex_string(const std::vector<uint8_t>& bytes) {
     std::ostringstream stream;
     for (uint8_t byte : bytes) {
-        stream << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(byte);
+        stream << std::hex << std::setw(2) << std::setfill('0')
+            << static_cast<int>(byte);
     }
     return stream.str();
 }
--- io.h
+++ io.h
@@ -5,7 +5,8 @@
 #include <cstdint>
 #include <map>
 
-std::map<std::string, std::string> parse_args(int argc, char** argv, const std::string& usage);
+std::map<std::string, std::string> parse_args(int argc, char** argv,
+    const std::vector<std::string>& accepted_args, const std::string& usage);
 std::vector<uint8_t> read_file_bytes(const std::string& filename);
 void write_file(const std::string& filename, const std::string& contents);
 std::string to_hex_string(const std::vector<uint8_t>& bytes);
